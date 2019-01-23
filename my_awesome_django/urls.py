f1c2998fd1b5eb699e8bd42b9efbd0a003281e81
Joshua kim <joshuakim.changer@gmail.com>
2019-01-23 11:15:04 +0900

    Fix Typo

--- my_awesome_django/urls.py
+++ my_awesome_django/urls.py
@@ -16,9 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from http_practice.views import root_view
-
 urlpatterns = [
-    path('', root_view),
     path('admin/', admin.site.urls),
 ]
